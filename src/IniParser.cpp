15730ac2814f0eb58508a3631910c1ab74f9a204
juanlu-ms <juanlums11@gmail.com>
2025-10-15 01:29:43 +0200

    Implemented destructor of IniParser

---
+++ src/IniParser.cpp
@@ -0,0 +1,19 @@
+#include "IniParser.h"
+
+IniParser::~IniParser() {
+    if (sections != nullptr) {
+        return;
+    }
+
+    for (int i = 0; i < size; ++i) {
+        for (int s = 0; s < sections[i].size; ++s) {
+            delete[] sections[i].data[s].key;
+            delete[] sections[i].data[s].value;
+        }
+        delete[] sections[i].data;
+        delete[] sections[i].name;
+    }
+
+    delete[] sections;
+}
+
